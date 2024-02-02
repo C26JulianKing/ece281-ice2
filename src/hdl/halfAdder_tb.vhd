8b9073248341166f8c57c2828a69eaee6ca92bed
C26JulianKing <github noreply email@users.noreply.github.com>
2024-02-01 21:56:55 -0700

    Half Added

--- src/hdl/halfAdder_tb.vhd
+++ src/hdl/halfAdder_tb.vhd
@@ -11,11 +11,11 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : halfAdder_tb.vhd
---| AUTHOR(S)     : Capt Warner
---| CREATED       : 01/17/2017
+--| AUTHOR(S)     : Julian King
+--| CREATED       : 01/23/2024
 --| DESCRIPTION   : This file implements a half adder test bench.
 --|
---| DOCUMENTATION : None
+--| DOCUMENTATION : No help received.
 --|
 --+----------------------------------------------------------------------------
 --|
@@ -61,8 +61,8 @@ architecture test_bench of halfAdder_tb is
 	port(
 		i_A     : in  std_logic; -- 1-bit input port
 		i_B     : in  std_logic; 
-		o_S     : out std_logic  -- 1-bit output port
-								 -- (NOTE: NO semicolon on LAST port only!)
+		o_S     : out std_logic;  -- 1-bit output port
+		o_Cout  : out std_logic -- (NOTE: NO semicolon on LAST port only!)
 		-- TODO:  Carry port
 	); -- the semicolon is here instead	
   end component;
@@ -71,11 +71,11 @@ architecture test_bench of halfAdder_tb is
   -- declare signals needed to stimulate the UUT inputs
   signal w_sw1 : std_logic := '0';
   -- TODO:  sw0 signal
-  
+  signal w_sw0 : std_logic := '0';
   -- also need signals for the outputs of the UUT
   signal w_led1 : std_logic := '0';
   -- TODO:  led0 signal
-
+  signal w_led0 : std_logic := '0';
   
 begin
 	-- PORT MAPS ----------------------------------------
@@ -84,7 +84,8 @@ begin
 	halfAdder_inst : halfAdder port map (
 		i_A     => w_sw1, -- notice comma (not a semicolon)
 		i_B     => w_sw0,
-		o_S     => w_led0 -- no comma on LAST one
+		o_S     => w_led0, -- no comma on LAST one
+		o_Cout  => w_led1
 		-- TODO:  map Cout 
 	);
 
@@ -100,6 +101,12 @@ begin
 	
 		 w_sw1 <= '0'; w_sw0 <= '0'; wait for 10 ns;
             assert w_led0 = '0' report "bad sum" severity error;
+            assert w_led1 = '0' report "bad carry" severity error;
+         w_sw1 <= '0'; w_sw0 <= '1'; wait for 10 ns;
+            assert w_led0 = '1' report "bad sum" severity error;
+            assert w_led1 = '0' report "bad carry" severity error;
+         w_sw1 <= '1'; w_sw0 <= '0'; wait for 10 ns;
+            assert w_led0 = '1' report "bad sum" severity error;
             assert w_led1 = '0' report "bad carry" severity error;
 		-- TODO:  rest of test plan
 		
